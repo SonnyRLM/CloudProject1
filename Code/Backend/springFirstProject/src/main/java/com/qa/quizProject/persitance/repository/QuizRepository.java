5d2ba232d544373d08e820f0c1d08b260c67dd0f
SonnyRLM <43782927+SonnyRLM@users.noreply.github.com>
2021-02-27 16:33:24 +0000

    Removed custom SQL query

--- Code/Backend/springFirstProject/src/main/java/com/qa/quizProject/persitance/repository/QuizRepository.java
+++ Code/Backend/springFirstProject/src/main/java/com/qa/quizProject/persitance/repository/QuizRepository.java
@@ -5,10 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.qa.quizProject.persitance.domain.QuizDescription;
 
 public interface QuizRepository extends JpaRepository<QuizDescription, Long> {
-	
-	//Run SELECT * FROM quiz_description WHERE quiz_name = 'something';
-	QuizDescription findQuizDescriptionByQuizName(String quizName);
-	
-	
+
 }
 
