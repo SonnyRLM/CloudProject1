c395ab0b57efeca46b256c3199517836c27ccb31
SonnyRLM <43782927+SonnyRLM@users.noreply.github.com>
2021-03-01 19:31:20 +0000

    removed unnecessary sql file

--- Code/Backend/springFirstProject/src/main/data.sql
+++
@@ -1,7 +0,0 @@
---INSERT INTO `band` (`NAME`) VALUES ('NAME OF BAND')
-
---INSERT INTO `musician` (`name`, `strings`, `type`,  `band_id`) VALUES ('STEVE', '5', 'long gitar', 'band_id') 
-
-INSERT INTO quiz_description (quiz_description, quiz_name) VALUES ('Select capital of each country', 'Capitals')
-
-INSERT INTO quiz_question (answers, correct, question, quiz_description_quiz_id) VALUES ('london;bath;manchester;suffolk', 'london', 'Capital of england', '1')
