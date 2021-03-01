dd95fae1f4e75f21a83a6ff9612d63f823cf8c71
SonnyRLM <43782927+SonnyRLM@users.noreply.github.com>
2021-03-01 18:54:43 +0000

    added profiles for test and dev

--- Code/Backend/springFirstProject/src/main/java/com/qa/quizProject/config/AppConfig.java
+++ Code/Backend/springFirstProject/src/main/java/com/qa/quizProject/config/AppConfig.java
@@ -3,8 +3,10 @@ package com.qa.quizProject.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile({ "test", "dev"})
 public class AppConfig {
 
 	@Bean
